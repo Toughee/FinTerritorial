e9c4b1c8bee2c7253a6cf1f64e1b35d250c989f0
Toughee <MJ1menez@protonmail.com>
2017-09-06 17:03:01 -0800

    Added Django support

--- js/datadisplay.js
+++
@@ -1,8 +0,0 @@
-function showData() {
-    var theSelect = Spigot.Dig;
-    var GDP = document.getElementById('GDP');
-
-    GDP.innerHTML = ('You have chosen ' + theSelect[theSelect.selectedIndex].text + '.'  
-    + ' Its Industry GDP is valued in ' + theSelect[theSelect.selectedIndex].value + ' billions.' 
-    + ' Its main industry is the ' + theSelect[theSelect.selectedIndex].id) + ' industry';
-}
