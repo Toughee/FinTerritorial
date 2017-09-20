9583e109837a29fbfc6e8e2ef9d684d2e5aee1a8
Toughee <MJ1menez@protonmail.com>
2017-09-19 22:12:16 -0800

    Added initializr. Made app responsive

--- stateselect/static/datadisplay.js
+++ stateselect/static/datadisplay.js
@@ -3,7 +3,7 @@ function showData() {
     var GDP = document.getElementById('GDP');
 
     GDP.innerHTML = ('You have chosen ' + theSelect[theSelect.selectedIndex].text + '.'  
-    + ' Its Industry GDP is valued in ' + 
+    + ' Its G.D.P is valued in ' + 
     theSelect[theSelect.selectedIndex].value + ' billions.' 
     + ' Its main industry is the ' + 
     theSelect[theSelect.selectedIndex].id) + ' industry';
