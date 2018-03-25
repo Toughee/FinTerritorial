d283557b66eb1a79f2a0d952bf2a3288b568b4ca
Toughee <MJ1menez@protonmail.com>
2018-03-24 21:34:23 -0500

    Updated settings for pip, django and python 3.6.4

--- SpigotDig/settings.py
+++ SpigotDig/settings.py
@@ -212,6 +212,10 @@ STATIC_URL = '/static/'
 
 ##########################################################################################
 
+##    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+##########################################################################################
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
